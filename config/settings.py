abbfb8380d1e80f6ea0b5c0f892db449421d360e
Robin Li <robin_0695@hotmail.com>
2019-11-17 22:40:59 +0800

    update mysql

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Celery settings
 
@@ -119,9 +121,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'thoth_django_dev',
+        'USER': 'thoth_django_dev',
+        'PASSWORD': 'thoth_django_dev',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
--- thoth_data_collector/views.py
+++ thoth_data_collector/views.py
@@ -6,6 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 from thoth_data_collector.models import PaperItem, PaperAuthor, IssueInfo
 from rest_framework import viewsets
 from thoth_data_collector.serializers import PaperItemSerializer, PaperAuthorSerializer, IssueInfoSerializer
+from rest_framework import generics
+
 
 
 class PaperViewSet(viewsets.ModelViewSet):
@@ -21,3 +23,15 @@ class PaperAuthorViewSet(viewsets.ModelViewSet):
 class IssueViewSet(viewsets.ModelViewSet):
     queryset = IssueInfo.objects.all().order_by('-id')
     serializer_class = IssueInfoSerializer
+
+
+class RecommandPaperList(generics.ListAPIView):
+    serializer_class = PaperItemSerializer
+
+    def get_queryset(self):
+        isRecommanded = self.request.query_params.get('is_recommanded', None)
+        if isRecommanded is not None:
+            queryset = PaperItem.objects.filter(is_recommanded=isRecommanded)
+        else:
+            queryset = PaperItem.objects.all()
+        return queryset
